30f50f5414a59cc35bfca7585fd90b3defe1d877
Termopsis <leo_dialog@mail.ru>
2020-10-06 00:16:36 +0300

    First commit in First branch

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        int[] arr = new int[10];
+        arr[0] = 10;
+        for(int b : arr){
+            System.out.println(b);
+        }
     }
 }
